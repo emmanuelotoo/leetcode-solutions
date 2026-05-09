3f5df0429ee6c62b111650a2cf8897019386db4b
emmanuelotoo <eotoo218@gmail.com>
2026-05-09 23:12:11 +0000

    Time: 11 ms (76.35%), Space: 19.3 MB (94.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+        # else:
+        #     return sorted(s) == sorted(t) brute force solution
+
         if len(s) != len(t):
             return False
 
-        return sorted(s) == sorted(t)
+        hashS, hashT = {}, {}
+
+        for i in range(len(s)):
+            hashS[s[i]] = 1 + hashS.get(s[i], 0)
+            hashT[t[i]] = 1 + hashT.get(t[i], 0)
+        for c in hashS:
+            if hashS[c] != hashT.get(c, 0):
+                return False
+ 
+        return True
+            
